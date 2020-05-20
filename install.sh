e89ae4d9cb399ad5dba796673493d3e1b7b63282
jdaeira <jdaeira@gmail.com>
2020-05-20 11:37:46 -0700

    Fix inistial script

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ DIR_LOGOS=$PWD"/Logos"
 DIR_FILES=$PWD"/Files"
 
 sudo cp $DIR_FILES"/gdm" /etc/dconf/profile
+sudo mkdir /usr/share/pixmaps/logo
 sudo cp $DIR_LOGOS"/New_POP_white.png" /usr/share/pixmaps/logo
+sudo mkdir /etc/dconf/db/gdm.d
 sudo cp $DIR_FILES"/01-logo" /etc/dconf/db/gdm.d
 
 sudo dconf update
